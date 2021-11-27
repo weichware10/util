118571f99d5ede901bf6116b8b4157b3ef3a4361
justinexner <92640370+justinexner@users.noreply.github.com>
2021-11-27 17:16:46 +0100

    Test Klasse fertig

--- src/test/java/github/weichware10/util/SpeicherUtilitiesTest.java
+++ src/test/java/github/weichware10/util/SpeicherUtilitiesTest.java
@@ -4,13 +4,66 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import github.weichware10.util.Enums.ToolType;
+
 /**
- * TODO: JUSTIN.
+ * Unit Test für Speicherutilities.
  */
 public class SpeicherUtilitiesTest {
+    /**
+     * Test if the Location is set correctly.
+     */
     @Test
     public void locationShouldBeSetCorrectly() {
-        SpeicherUtilities instance = new SpeicherUtilities("7");
-        assertEquals("7", instance.getLocation());
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        assertEquals("C/Documents", instance.getLocation());
+    }
+    /**
+     * Test if the Location is returned correctly.
+     */
+
+    @Test
+    public void gotLocationCorrect() {
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        assertEquals("C/Documents", instance.getLocation());
+    }
+    /**
+     * Test if the Location is changed correctly.
+     */
+
+    @Test
+    public void locationShouldBeChangedCorrectly() {
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        instance.setLocation("C/Music");
+        assertEquals("C/Music", instance.getLocation());
+    }
+    /**
+     * Test if the deletion was successfull.
+     */
+
+    @Test
+    public void deletionWasSuccessfull() {
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        assertEquals(true, instance.deleteData("Versuch1 Eyetracking"));
+    }
+    /**
+     * Test if the search was successfull.
+     */
+
+    @Test
+    public void searchWasSuccessfull() {
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        boolean test = instance.searchData("Versuch1 ZoomMaps") instanceof Data;
+        assertEquals(true, test);
+    }
+    /**
+     * Test if the save was successfull.
+     */
+
+    @Test
+    public void saveWasSuccessfull(){
+        SpeicherUtilities instance = new SpeicherUtilities("C/Documents");
+        Data versuch2ZoomMaps = new Data(ToolType.ZOOM, 2);
+        assertEquals(true, instance.saveData(versuch2ZoomMaps));
     }
 }
