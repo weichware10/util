9416ebe2ec9db543ca4069ef97e611cd5596995b
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-03 16:17:39 +0100

    [feat] write Configuration to JSON

--- src/main/java/github/weichware10/util/config/ConfigWriter.java
+++ src/main/java/github/weichware10/util/config/ConfigWriter.java
@@ -1,9 +1,17 @@
 package github.weichware10.util.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 /**
  * statische Klasse zum Speichern der Konfiguration.
  */
-final class ConfigWriter {
+public final class ConfigWriter {
 
     /**
      * Cannot be instantiated.
@@ -13,13 +21,37 @@ final class ConfigWriter {
     }
 
     /**
+     * Speichert eine {@link Configuration} in einer JSON-Datei.
+     *
+     * @param location - Speicherort der JSON-Datei
+     * @param configuration - die abzuspeichernde Configuration
+     * @return Erfolgsboolean
+     */
+    public static boolean toJson(String location, Configuration configuration) {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            String json = ow.writeValueAsString(configuration);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(location, false));
+            writer.append(json);
+            writer.close();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * Speicher eine Konfiguration ab.
      *
      * @param location - der gewünschte Speicherort
      * @param configuration - die zu speichernde Konfiguration
      * @return Erfolgsboolean
      */
-    public static boolean writeConfiguration(String location, Configuration configuration) {
+    public static boolean toDataBase(String location, Configuration configuration) {
         if (location == "www.weichware10.com/config") {
             return true;
         } else {
--- src/test/java/github/weichware10/util/config/ConfigWriterTest.java
+++ src/test/java/github/weichware10/util/config/ConfigWriterTest.java
@@ -3,7 +3,9 @@ package github.weichware10.util.config;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import github.weichware10.util.Enums.ToolType;
 import org.junit.Test;
+
 
 /**
  * Testet {@link ConfigWriter}.
@@ -12,8 +14,16 @@ public class ConfigWriterTest {
     @Test
     public void onlyWriteToValidLocation() {
         assertFalse("ConfigWriter should not write to shady locations.",
-            ConfigWriter.writeConfiguration("www.downloadfreeramNOW.com", new Configuration()));
+                ConfigWriter.toDataBase(
+                        "www.downloadfreeramNOW.com", new Configuration(ToolType.ZOOMMAPS)));
         assertTrue("ConfigWriter should write to valid locations.",
-            ConfigWriter.writeConfiguration("www.weichware10.com/config", new Configuration()));
+                ConfigWriter.toDataBase(
+                        "www.weichware10.com/config", new Configuration(ToolType.ZOOMMAPS)));
+    }
+
+    @Test
+    public void testWriteToJson() {
+        ConfigWriter.toJson("location", new Configuration(ToolType.ZOOMMAPS));
+        assertTrue(true);
     }
 }
