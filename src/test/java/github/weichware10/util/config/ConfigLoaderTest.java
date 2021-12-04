11a54c959e272d4f92691a72574d2370dc6c2687
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-04 14:42:31 +0100

    [test] ConfigLoader Test

--- src/test/java/github/weichware10/util/config/ConfigLoaderTest.java
+++ src/test/java/github/weichware10/util/config/ConfigLoaderTest.java
@@ -1,10 +1,13 @@
 package github.weichware10.util.config;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
+import github.weichware10.util.Enums.ToolType;
 import org.junit.Test;
+
 
 /**
  * Testet {@link ConfigLoader}.
@@ -24,5 +27,26 @@ public class ConfigLoaderTest {
         assertThat("loadConfiguration should return instance of Configuration class",
                 ConfigLoader.fromDataBase("www.weichware10.com/config"),
                 instanceOf(Configuration.class));
+    }
+
+    /**
+     * Testet, ob das Laden aus einer Datei erfolgreich ist,
+     * indem erst eine Datei abgespeichert wird, diese dann neu geladen wird
+     * und die zwei Konfigurationen miteinander verglichen werden.
+     */
+    @Test
+    public void loadingIsSuccesfull() {
+        Configuration configC1 = new Configuration(ToolType.CODECHARTS);
+        Configuration configE1 = new Configuration(ToolType.EYETRACKING);
+        Configuration configZ1 = new Configuration(ToolType.ZOOMMAPS);
+        ConfigWriter.toJson("target/configC1.json", configC1);
+        ConfigWriter.toJson("target/configE1.json", configE1);
+        ConfigWriter.toJson("target/configZ1.json", configZ1);
+        Configuration configC2 = ConfigLoader.fromJson("target/configC1.json");
+        Configuration configE2 = ConfigLoader.fromJson("target/configE1.json");
+        Configuration configZ2 = ConfigLoader.fromJson("target/configZ1.json");
+        assertEquals(configC1, configC2);
+        assertEquals(configE1, configE2);
+        assertEquals(configZ1, configZ2);
     }
 }
