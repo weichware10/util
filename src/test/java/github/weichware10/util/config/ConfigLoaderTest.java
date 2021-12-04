066a3b1df30981809cd9d98aac35970d3ee9298b
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-04 16:50:25 +0100

    [test] Tests für ConfigLoader angepasst

--- src/test/java/github/weichware10/util/config/ConfigLoaderTest.java
+++ src/test/java/github/weichware10/util/config/ConfigLoaderTest.java
@@ -31,22 +31,27 @@ public class ConfigLoaderTest {
 
     /**
      * Testet, ob das Laden aus einer Datei erfolgreich ist,
-     * indem erst eine Datei abgespeichert wird, diese dann neu geladen wird
-     * und die zwei Konfigurationen miteinander verglichen werden.
+     * indem Testkonfigurationen doppelt geladen werden und dann miteinander verglichen werden.
      */
     @Test
     public void loadingIsSuccesfull() {
-        Configuration configC1 = new Configuration(ToolType.CODECHARTS);
-        Configuration configE1 = new Configuration(ToolType.EYETRACKING);
-        Configuration configZ1 = new Configuration(ToolType.ZOOMMAPS);
-        ConfigWriter.toJson("target/configC1.json", configC1);
-        ConfigWriter.toJson("target/configE1.json", configE1);
-        ConfigWriter.toJson("target/configZ1.json", configZ1);
-        Configuration configC2 = ConfigLoader.fromJson("target/configC1.json");
-        Configuration configE2 = ConfigLoader.fromJson("target/configE1.json");
-        Configuration configZ2 = ConfigLoader.fromJson("target/configZ1.json");
-        assertEquals(configC1, configC2);
-        assertEquals(configE1, configE2);
-        assertEquals(configZ1, configZ2);
+        // Version 1 laden
+        Configuration configC1 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-CODECHARTS.json");
+        Configuration configE1 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-EYETRACKING.json");
+        Configuration configZ1 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-ZOOMMAPS.json");
+        // Version 2 laden
+        Configuration configC2 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-CODECHARTS.json");
+        Configuration configE2 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-EYETRACKING.json");
+        Configuration configZ2 = ConfigLoader.fromJson(
+                "src/test/resources/testconfig-ZOOMMAPS.json");
+        //  Vergleichen
+        assertEquals("Configs should be the same", configC1, configC2);
+        assertEquals("Configs should be the same", configE1, configE2);
+        assertEquals("Configs should be the same", configZ1, configZ2);
     }
 }
