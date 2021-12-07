59632c57b7099c790742347cbe722a35c8b9c769
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-07 15:26:41 +0100

    [feat] ausgreiftes data package
    Co-authored-by: TheDarkDesync <TheDarkDesync@users.noreply.github.com>

--- src/main/java/github/weichware10/util/data/DataPoint.java
+++ src/main/java/github/weichware10/util/data/DataPoint.java
@@ -1,55 +1,69 @@
 package github.weichware10.util.data;
 
+import java.util.Arrays;
+
 /**
  * Stores a single DataPoint.
+ *
+ * @since v0.2
  */
 public class DataPoint {
     public final int dataId;
-    public final float timeOffset; // TODO: unit!
+    public final int timeOffset;
     public final int[] coordinates; // ! int[2]
     public final int[] rasterSize; // ! int[2]
-    private float zoomLevel;
+    public final Float zoomLevel;
 
     /**
      * Stores a single DataPoint with zoomLevel.
      *
-     * @param dataId - the id of the dataPoint
-     * @param timeOffset - the time since the trial started
+     * @param dataId      - the id of the dataPoint
+     * @param timeOffset  - the time since the trial started
      * @param coordinates - the coordinates of the viewed picture
-     * @param rasterSize - width and height of the raster
-     * @param zoomLevel - the zoom strength // TODO: besser Beschreibung finden
+     * @param zoomLevel   - how far the user is zoomed in
+     *
+     * @since v0.2
      */
-    public DataPoint(int dataId, float timeOffset, int[] coordinates, int[] rasterSize,
-            float zoomLevel) {
+    protected DataPoint(int dataId, int timeOffset, int[] coordinates, float zoomLevel) {
         this.dataId = dataId;
         this.timeOffset = timeOffset;
         this.coordinates = coordinates;
-        this.rasterSize = rasterSize;
+        this.rasterSize = null;
         this.zoomLevel = zoomLevel;
     }
 
     /**
      * Stores a single DataPoint without zoomLevel.
      *
-     * @param dataId - the id of the dataPoint
-     * @param timeOffset - the time since the trial started
-     * @param coordinates - the coordinates of the viewed picture
-     * @param rasterSize - width and height of the raster
+     * @param dataId      - the id of the dataPoint
+     * @param timeOffset  - the time since the trial started
+     * @param coordinates - the coordinates on the viewed picture
+     * @param rasterSize  - width and height of the raster
+     *
+     * @since v0.2
      */
-    public DataPoint(int dataId, float timeOffset, int[] coordinates, int[] rasterSize) {
+    protected DataPoint(int dataId, int timeOffset, int[] coordinates, int[] rasterSize) {
         this.dataId = dataId;
         this.timeOffset = timeOffset;
         this.coordinates = coordinates;
         this.rasterSize = rasterSize;
+        this.zoomLevel = null;
     }
 
-    /**
-     * get the saved zoomLevel.
-     *
-     * @return the saved zoomLevel
-     */
-    public float getZoomLevel() {
-        return zoomLevel;
+    @Override
+    public String toString() {
+        return String.format("""
+                DataPoint: {
+                    dataId: %d,
+                    timeOffset: %d,
+                    coordinates: %s,
+                    rasterSize: %s,
+                    zoomLevel: %s
+                }""",
+                dataId,
+                timeOffset,
+                Arrays.toString(coordinates),
+                Arrays.toString(rasterSize),
+                (zoomLevel == null) ? "null" : zoomLevel.toString());
     }
-
 }
--- src/main/java/github/weichware10/util/data/TrialData.java
+++ src/main/java/github/weichware10/util/data/TrialData.java
@@ -1,33 +1,39 @@
 package github.weichware10.util.data;
 
 import github.weichware10.util.Enums;
+import github.weichware10.util.Enums.ToolType;
 import java.util.ArrayList;
+import java.util.List;
 import org.joda.time.DateTime;
 
 /**
  * Stores the TrialData for the different tools internally.
  *
  * <p>also used to transfer Data
+ *
+ * @since v0.2
  */
 public class TrialData {
-    public final Enums.ToolType tooltype;
+    public final Enums.ToolType toolType;
     public final String trialId;
     public final String configId;
     public final DateTime startTime;
-    private ArrayList<DataPoint> dataPoints;
+    private List<DataPoint> dataPoints;
 
     /**
      * Stores the TrialData for the different tools internally.
      *
-     * @param tooltype - the tooltype of the stored data
-     * @param trialId - the id of the trial
+     * @param toolType - the tooltype of the stored data
+     * @param trialId  - the id of the trial
      * @param configId - the configuration of the stored data
+     *
+     * @since v0.2
      */
-    public TrialData(Enums.ToolType tooltype, String trialId, String configId) {
-        this.tooltype = tooltype;
+    public TrialData(Enums.ToolType toolType, String trialId, String configId) {
+        this.toolType = toolType;
         this.trialId = trialId;
         this.configId = configId;
-        this.startTime = DateTime.now(); // TODO: Wann wird Instanz im Versuch erzugt?
+        this.startTime = DateTime.now();
         this.dataPoints = new ArrayList<DataPoint>();
     }
 
@@ -35,17 +41,80 @@ public class TrialData {
      * get the stored dataPoints.
      *
      * @return the stored dataPoints
+     *
+     * @since v0.2
      */
-    public ArrayList<DataPoint> getData() {
-        return this.dataPoints;
+    public List<DataPoint> getData() {
+        return dataPoints;
     }
 
     /**
-     * set new dataPoints.
+     * Add a DataPoint for CodeCharts.
      *
-     * @param dataPoints - dataPoints to be set
+     * @param coordinates - the coordinates on the viewed picture
+     * @param rasterSize  - width and height of the raster
+     *
+     * @since v0.2
      */
-    public void setData(ArrayList<DataPoint> dataPoints) {
-        this.dataPoints = dataPoints;
+    public void addDataPoint(int[] coordinates, int[] rasterSize) {
+
+        if (toolType != ToolType.CODECHARTS) {
+            throw new IllegalArgumentException("Can only add CODECHARTS DataPoints.");
+        }
+
+        if (coordinates.length != 2) {
+            throw new IllegalArgumentException("coordinates[] needs to have a length of 2");
+        }
+        if (rasterSize.length != 2) {
+            throw new IllegalArgumentException("rasterSize[] needs to have a length of 2");
+        }
+
+        // kann "ohne" Bedenken gecastet werden,
+        // damit Overflow auftritt, müsste zwischen Anfang und jetzt ca 25 Tage liegen.
+        int timeOffset = (int) (DateTime.now().getMillis() - startTime.getMillis());
+
+        dataPoints.add(new DataPoint(dataPoints.size(), timeOffset, coordinates, rasterSize));
+    }
+
+    /**
+     * Add a DataPoint for CodeCharts.
+     *
+     * @param coordinates - the coordinates on the viewed picture
+     * @param zoomLevel   - how far the user is zoomed in
+     *
+     * @since v0.2
+     */
+    public void addDataPoint(int[] coordinates, float zoomLevel) {
+
+        if (toolType != ToolType.ZOOMMAPS) {
+            throw new IllegalArgumentException("Can only add ZOOMMAPS DataPoints.");
+        }
+
+        if (coordinates.length != 2) {
+            throw new IllegalArgumentException("coordinates[] needs to have a length of 2");
+        }
+
+        // kann "ohne" Bedenken gecastet werden,
+        // damit Overflow auftritt, müsste zwischen Anfang und jetzt ca 25 Tage liegen.
+        int timeOffset = (int) (DateTime.now().getMillis() - startTime.getMillis());
+
+        dataPoints.add(new DataPoint(dataPoints.size(), timeOffset, coordinates, zoomLevel));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                TrialData: {
+                    toolType: %s
+                    trialId: %s
+                    configId: %s
+                    startTime: %s
+                    dataPoints: dataPoints[%d]
+                }""",
+                toolType.toString(),
+                trialId,
+                configId,
+                startTime.toString(),
+                dataPoints.size());
     }
 }
