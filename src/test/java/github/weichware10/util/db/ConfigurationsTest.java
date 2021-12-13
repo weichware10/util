5c7bded7538f50fa3d333efede6aa750b740727d
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-13 20:52:46 +0100

    [test] Configuration

---
+++ src/test/java/github/weichware10/util/db/ConfigurationsTest.java
@@ -0,0 +1,93 @@
+package github.weichware10.util.db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import github.weichware10.util.config.CodeChartsConfiguration;
+import github.weichware10.util.config.Configuration;
+import github.weichware10.util.config.ZoomMapsConfiguration;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Arrays;
+import org.junit.Test;
+
+/**
+ * Testet Configurations.java
+ */
+public class ConfigurationsTest {
+
+    final DataBaseClient dbClient;
+    Configuration ccConfig;
+    Configuration zmConfig;
+    Configuration zmConfigWoTu;
+
+    /**
+     * Erstellen des DataBaseClients.
+     */
+    public ConfigurationsTest() {
+        Dotenv dotenv = Dotenv.load();
+        dbClient = new DataBaseClient(
+                dotenv.get("DB_URL"),
+                dotenv.get("DB_USERNAME"),
+                dotenv.get("DB_PASSWORD"),
+                dotenv.get("DB_SCHEMA"));
+        dbClient.clearTestSchema();
+
+        CodeChartsConfiguration codeChartsConfiguration = new CodeChartsConfiguration(
+                Arrays.asList("test1", "test2"),
+                new int[] { 20, 40 },
+                new long[] { 200, 400 },
+                true,
+                Arrays.asList("url1", "url2"));
+
+        ccConfig = new Configuration("dunno yet", "Test Question?", codeChartsConfiguration);
+        String ccConfigId = dbClient.configurations.setConfiguration(ccConfig);
+        ccConfig = new Configuration(ccConfigId, "Test Question?", codeChartsConfiguration);
+
+        ZoomMapsConfiguration zoomMapsConfiguration1 = new ZoomMapsConfiguration(4.2f, true,
+                Arrays.asList("url1", "url2", "url3"));
+
+        zmConfig = new Configuration("dunno yet", "Test Question?", zoomMapsConfiguration1);
+        String zmConfigId = dbClient.configurations.setConfiguration(zmConfig);
+        zmConfig = new Configuration(zmConfigId, "Test Question?", zoomMapsConfiguration1);
+
+        ZoomMapsConfiguration zoomMapsConfiguratiom2 = new ZoomMapsConfiguration(4.2f, false,
+                Arrays.asList("url1", "url2", "url3"));
+
+        zmConfigWoTu = new Configuration("dunno yet", "Test Question?", zoomMapsConfiguratiom2);
+        String zmConfigWoTuId = dbClient.configurations.setConfiguration(zmConfigWoTu);
+        zmConfigWoTu = new Configuration(zmConfigWoTuId, "Test Question?", zoomMapsConfiguratiom2);
+    }
+
+    @Test
+    public void setConfigurationsShouldWork() {
+        CodeChartsConfiguration codeChartsConfiguration = new CodeChartsConfiguration(
+                Arrays.asList("test1", "test2"),
+                new int[] { 20, 40 },
+                new long[] { 200, 400 },
+                true,
+                Arrays.asList("url1", "url2"));
+
+        Configuration config;
+        String configId;
+
+        config = new Configuration("", "Test Question?", codeChartsConfiguration);
+        configId = dbClient.configurations.setConfiguration(config);
+        assertTrue("configId sollte mit con_ anfangen.", configId.startsWith("con_"));
+
+        ZoomMapsConfiguration zoomMapsConfiguration = new ZoomMapsConfiguration(4.2f, true,
+                Arrays.asList("url1", "url2", "url3"));
+        config = new Configuration("", "Test Question?", zoomMapsConfiguration);
+        configId = dbClient.configurations.setConfiguration(config);
+        assertTrue("configId sollte mit con_ anfangen.", configId.startsWith("con_"));
+    }
+
+    @Test
+    public void getConfigurationsShouldWork() {
+        assertEquals(ccConfig,
+                dbClient.configurations.getConfiguration(ccConfig.getConfigId()));
+        assertEquals(zmConfig,
+                dbClient.configurations.getConfiguration(zmConfig.getConfigId()));
+        assertEquals(zmConfigWoTu,
+                dbClient.configurations.getConfiguration(zmConfigWoTu.getConfigId()));
+    }
+}
