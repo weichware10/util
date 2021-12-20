eab36f52be990eb4e1104059badb8752ddf99e5d
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-20 20:18:11 +0100

    [test] Tests angepasst

---
+++ src/test/java/github/weichware10/util/config/ConfigurationTest.java
@@ -0,0 +1,23 @@
+package github.weichware10.util.config;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Testen Configuration
+ */
+public class ConfigurationTest {
+
+    @Test
+    public void toStringWorks() {
+        Configuration ccc = new Configuration("con_id", "question?",
+                new CodeChartsConfiguration());
+        Configuration zmc = new Configuration("con_id", "question?",
+                new ZoomMapsConfiguration());
+
+        assertTrue("toString should match", zmc.toString().matches("Configuration: [\\s\\S]+"));
+        assertTrue("toString should match", zmc.toString().matches("Configuration: [\\s\\S]+"));
+    }
+
+}
