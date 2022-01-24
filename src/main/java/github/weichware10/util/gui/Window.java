a464d767b51009bab1a45863418d6588890f8b41
Rotwolf <carl.jonathan.wolf@gmx.de>
2022-01-24 14:05:23 +0100

    [feat]reload Methode

---
+++ src/main/java/github/weichware10/util/gui/Window.java
@@ -0,0 +1,31 @@
+package github.weichware10.util.gui;
+
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+/**
+ * Beinhaltet Refresh Methode.
+ */
+public class Window {
+
+    /**
+     * Lösung für die Scaling Probleme.
+     * Verändert Breite um +1 und dann um -1, was zu einem Refresh führt.
+     *
+     * @param primaryStage - Stage who gets refreshed.
+     */
+    public static void refresh(Stage primaryStage) {
+
+        primaryStage.setWidth(primaryStage.getWidth() - 1);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Platform.runLater(() -> primaryStage.setWidth(primaryStage.getWidth() + 1));
+            }
+
+        }).start();
+    }
+
+}
